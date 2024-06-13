b65677af92a03e1d7942b8188b403d8ac061d895
Tobias E. Naegele <161582635+tnaegele@users.noreply.github.com>
2024-06-14 00:16:38 +0100

    fix wrong package name

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
-from .bulk_hide_silkscreen_designators_action import CutTracksAtLine
+from .cut_tracks_action import CutTracksAtLine
 CutTracksAtLine().register() # Instantiate and register to Pcbnew
